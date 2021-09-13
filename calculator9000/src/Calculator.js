47ee7b5fbf9f5b5629a181ef3ebde56ebe353f4f
ahcene-kadi <you@example.com>
2021-09-13 16:12:57 +0200

    creation fichier components

---
+++ calculator9000/src/Calculator.js
@@ -0,0 +1,10 @@
+import './App.css';
+import TheTitle from './TheTitle';
+
+function Calculator() {
+  return (
+    <TheTitle />
+  );
+}
+
+export default Calculator;
---
+++ calculator9000/src/TheTitle.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function TheTitle() {
+  return (
+    <div className="TheTitle">
+      <h1>Calculator 9000</h1>
+    </div>
+  );
+}
+
+export default TheTitle;
